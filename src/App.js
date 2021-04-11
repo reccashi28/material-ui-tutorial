b6e350c21134aedb220fc9f103af244ae94d5f03
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-04-11 20:39:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="contained" color="secondary">Hello World</Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
